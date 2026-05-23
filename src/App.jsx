6b92c9562a97d53a3311231a1f344928f230779b
rokeyasultana <rokeyasbd@gmail.com>
2026-05-23 10:54:28 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Home from '../src/Components/pages/Home/Home'
+function App() {
+
+  return (
+    <>
+       <h1 className="text-5xl text-red-300 font-bold text-center mt-10">
+      Tailwind Working
+      <Home></Home>
+    </h1>
+    </>
+  )
+}
+
+export default App
